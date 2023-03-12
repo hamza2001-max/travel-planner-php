347dd55cb31cfe73ed5f1ab6b7326a647794f452
hamza2001-max <ellawliet1007@gmail.com>
2023-03-12 19:58:21 +0500

    made some changes in the destinationDetail.html and its js file.

---
+++ frontend/JSON/data.js
@@ -0,0 +1,53 @@
+// import value from "../staticModels/"
+export const destinationArray = [
+    {
+        id: 0, name: "Malaysia", image: "./staticModels/malaysia.jpg",
+        landmarks: [{
+            name: "Redang Island",
+            landmarkImage: "./staticModels/malaysiaIsland.jpg"
+        },
+        {
+            name: "Petronas Towers",
+            landmarkImage: "./staticModels/malaysiaTower.jpg"
+        },
+        {
+            name: "Sultan Masjid",
+            landmarkImage: "./staticModels/malaysiaMasjid.jpg"
+        },
+        {
+            name: "Royal Park",
+            landmarkImage: "./staticModels/malaysiaWaterfall.jpg"
+        }]
+        , price: 572,
+        description: "Malaysia is a beautiful Southeast Asian country located on the Malay Peninsula and the island of Borneo. It is a diverse country, known for its mix of modern cities, stunning natural landscapes, rich culture, and delicious cuisine."
+    },
+    {
+        id: 1, name: "Russia", image: "./staticModels/russia.jpg", landmarks: [{
+            name: "Redang Island",
+            landmarkImage: "./staticModels/malaysiaIsland.jpg"
+        },
+        {
+            name: "Petronas Towers",
+            landmarkImage: "./staticModels/malaysiaTower.jpg"
+        },
+        {
+            name: "Sultan Masjid",
+            landmarkImage: "./staticModels/malaysiaMasjid.jpg"
+        },
+        {
+            name: "Royal Park",
+            landmarkImage: "./staticModels/malaysiaWaterfall.jpg"
+        }]
+        , price: 572,
+        description: "Malaysia is a beautiful Southeast Asian country located on the Malay Peninsula and the island of Borneo. It is a diverse country, known for its mix of modern cities, stunning natural landscapes, rich culture, and delicious cuisine."
+    },
+    // { id: 1, name: 'Russia', image: './staticModels/russia.jpg' },
+    // { id: 2, name: 'South Korea', image: './staticModels/korea.jpg' },
+    // { id: 3, name: 'Mongolia', image: './staticModels/mongolia.jpg' },
+    // { id: 4, name: 'Italy', image: './staticModels/italy.jpg' },
+    // { id: 5, name: 'Peru', image: './staticModels/peru.jpg' },
+    // { id: 6, name: 'Brazil', image: './staticModels/brazil.jpg' },
+    // { id: 7, name: 'Argentina', image: './staticModels/argentina.jpg' },
+    // { id: 8, name: 'South Africa', image: './staticModels/southAfrica.jpg' },
+    // { id: 9, name: 'Thailand', image: './staticModels/thailand.jpg' }
+];
--- frontend/index.js
+++ frontend/index.js
@@ -1,27 +1,11 @@
-//json data
-const destinationArray = [
-    {
-        id: 0, name: 'Malaysia', image: './staticModels/malaysia.jpg', price: 572,
-        description: "Malaysia is a beautiful Southeast Asian country located on the Malay Peninsula and the island of Borneo. It is a diverse country, known for its mix of modern cities, stunning natural landscapes, rich culture, and delicious cuisine."
-    },
-    // { id: 1, name: 'Russia', image: './staticModels/russia.jpg' },
-    // { id: 2, name: 'South Korea', image: './staticModels/korea.jpg' },
-    // { id: 3, name: 'Mongolia', image: './staticModels/mongolia.jpg' },
-    // { id: 4, name: 'Italy', image: './staticModels/italy.jpg' },
-    // { id: 5, name: 'Peru', image: './staticModels/peru.jpg' },
-    // { id: 6, name: 'Brazil', image: './staticModels/brazil.jpg' },
-    // { id: 7, name: 'Argentina', image: './staticModels/argentina.jpg' },
-    // { id: 8, name: 'South Africa', image: './staticModels/southAfrica.jpg' },
-    // { id: 9, name: 'Thailand', image: './staticModels/thailand.jpg' }
-];
+import { destinationArray } from "./JSON/data.js";
 
-//index.html's js
 //mapping through the destinations
 const destinationContainer = document.getElementById("destinationContainer");
 const mappedDestinations = destinationArray.map((destination) => {
     return (
         `<div class="destinationItem" key=${destination.id}
-         onclick="redirectToDetail('${destination.name}', '${destination.image}', '${destination.description}', '${destination.price}')">
+         onclick="redirectToDetail('${destination.id}')">
          <div style="position: relative;">
             <div class="imageWrapper">
                 <h3 class="imageWrapperText">Discover ${destination.name}</h3>
@@ -35,10 +19,10 @@ const mappedDestinations = destinationArray.map((destination) => {
     );
 });
 destinationContainer.innerHTML = mappedDestinations.join('');
+
 //redirecting to another page
-function redirectToDetail(name, image, description, price) {
-    const url = `destinationDetails.html?name=${name}&image=${image}&description=${description}&price=${price}`;
+window.redirectToDetail = function(id) {
+    const url = `destinationDetails.html?id=${id}`;
     window.location.href = url;
 }
 
-//destinationDetail's js
